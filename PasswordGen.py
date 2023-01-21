875960f66bbbe7265df2773961d2d1e59af9e678
WheatelyTech <123122039+Wheately@users.noreply.github.com>
2023-01-21 15:21:06 +0100

    Fancier PassGen
    Added notes for the editors, and added a saving system.
    (Saving system might glitch out if there's a file named Password.txt in the directory the program is run in.)

--- PasswordGen.py
+++ PasswordGen.py
@@ -1,18 +1,44 @@
 import random
 from time import *
+from datetime import datetime
+import getpass
+username = getpass.getuser()
 
+#Other stuff
+current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+
+#Intro
 print("Welcome to PasswordGen! This is a very simple open-source program that quickly generates a 20-character password that works everywhere.")
 sleep(1)
 
+#Variables
 lowercase = "abcdefghijklmnoprstquvxyz"
 uppercase = "ABCDEFGHIJKLMNOPRSTQUVXYZ"
 numbers = "1234567890"
 symbols = "!@#$^%&*{}[];/,._-"
+mixmash = "KkhJGFg884G'\[;'."
+special = "ẞαλ§"
 
-all = lowercase + uppercase + numbers + symbols
+#Mixing and creating password
+all = lowercase + uppercase + numbers + symbols + mixmash + special
 length = 20
 password = "".join(random.sample(all, length))
 
+#Printing password 
 print("Your randomly-generated password is " + password)
+sleep(1)
+save = input("Do you want to save the password? (y/n)")
+#Log system
+if save == "y":
+    with open("Password.txt", "w") as file:
+        file.write("Password saved by " + username + " at " + current_datetime + " is " + password)
+    print("Password saved to Password.txt.")
+    sleep(1)
+    quit()
+else:
+    print("Password not saved,")
+    sleep(1)
+    quit
 
 sleep(100)
